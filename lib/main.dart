6616cb53d12eaa7fc1721c9f83ce436ec63ba0d4
mudassir666 <mudassir666666666@gmail.com>
2021-07-27 00:57:42 +0500

    part2_completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,103 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        brightness: Brightness.dark,
+        primaryColor: Colors.purple[900],
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      title: "WordPair",
+      home: RandomWords(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
+class RandomWords extends StatefulWidget {
+  const RandomWords({
+    Key? key,
+  }) : super(key: key);
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _RandomWordsState createState() => _RandomWordsState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  TextEditingController _mep = TextEditingController();
-  Map map = {};
-  var no = 0;
-
-  // void _incrementCounter() {
-  //   setState(() {
-  //     _counter++;
-  //   });
-  // }
-
+class _RandomWordsState extends State<RandomWords> {
   @override
   Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Center(child: Text("WordPair Generator")),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            TextFormField(
-              controller: _mep,
-              decoration: InputDecoration(
-                  labelText: "Name",
-                  hintText: "Enter Name",
-                  icon: Icon(Icons.account_box_sharp)),
-            )
-          ],
-        ),
+        child: Text(wordPair.asPascalCase),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          
-         
-          setState(() {
-            map[no] = _mep.text;
-            no++;
-           print(map);
-          });
-        },
-        child: Icon(Icons.add),
-      ),
-      // floatingActionButton: FloatingActionButton(
-      //   onPressed: _incrementCounter,
-      //   tooltip: 'Increment',
-      //   child: Icon(Icons.add),
-      // ),
-      drawer: Drawer(
-        child: map.length != 0
-            ? ListView.builder(
-                // padding: EdgeInsets.zero,
-                // gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    title: Text(
-                      map[index],
-                      style:
-                          TextStyle(fontStyle: FontStyle.italic, fontSize: 18),
-                    ),
-                    tileColor: Colors.amber[100],
-                  );
-                },
-                itemCount: map.length,
-              )
-            : Center(
-                child: CircularProgressIndicator(),
-              ),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
